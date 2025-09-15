eff7f2dd13675ef6b1c64f999001b5d0b03fb28b
Anju-kimura <172464703+Anju-kimura@users.noreply.github.com>
2025-09-15 05:58:17 +0000

    [Add]いろいろついか

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'homes/about' => 'homes#about', as: 'about'
-  resources :users, only:[:show, :edit, :index, :new]
-  resources :books, only:[:index, :edit, :show, :new]
+  get 'home/about' => 'homes#about', as: 'about'
+  resources :users, only:[:show, :edit, :index, :new, :update]
+  resources :books, only:[:index, :edit, :show, :new, :create, :update, :destroy]
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
